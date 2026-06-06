e8da6bf453aad719e4cca10ecd17d84651b5233e
Kostas Kokkoros <mavroprovato@gmail.com>
2026-06-06 03:05:31 +0300

    Add example chart

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,57 @@
 <script setup lang="ts">
-import {ref} from 'vue'
+import { ref } from 'vue'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { Line } from 'vue-chartjs'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+const data = ref({
+  labels: ['One', 'Two', 'Three', 'Four'],
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: [1, 2, 3, 4],
+    },
+    {
+      label: 'Dataset 2',
+      data: [1.2, 2.2, 3.3, 4.4]
+    }
+  ]
+});
+
+const options = {
+  responsive: true,
+  maintainAspectRatio: false
+};
 
 const url = 'http://localhost:8000/api/data/daily/country';
-const data = ref([]);
 fetch(url)
     .then(response => response.json())
     .then(responseData => {
-      data.value = responseData;
+      console.log(responseData);
     });
 </script>
 
 <template>
   <h1>Fuel prices</h1>
-  <p>{{ data }}</p>
+  <p><Line :data="data" :options="options" /></p>
 </template>
 
 <style scoped></style>
