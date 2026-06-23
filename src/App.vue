75a7e706bd21e2973cd95445b133d212b4d78311
Kostas Kokkoros <mavroprovato@gmail.com>
2026-06-24 01:40:51 +0300

    Refactor chart to a child component

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,14 @@
 <script setup lang="ts">
-import {onMounted, ref, watch} from 'vue'
+import {onMounted, ref} from 'vue'
 import {VueDatePicker} from '@vuepic/vue-datepicker';
-import {API} from "@/services/api.ts";
-import 'chartjs-adapter-moment';
-import {Line} from 'vue-chartjs'
-import '@vuepic/vue-datepicker/dist/main.css'
-import {
-  CategoryScale, Chart as ChartJS, Legend, LinearScale, LineElement, PointElement, Title, Tooltip, TimeScale
-} from 'chart.js'
-// Register ChartJS components
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, TimeScale);
+import {API} from './services/api.ts';
+import DailyCountryData from './components/DailyCountryData.vue';
 
 const dateRange = ref({
   minDate: null,
   maxDate: null,
 });
-const dates = ref();
-
-const chartOptions = ref({
-  responsive: true,
-  maintainAspectRatio: false,
-  pointStyle: false,
-  spanGaps: true,
-  scales: {
-    x: {
-      type: 'time',
-      grid: {
-        display: false
-      }
-    },
-  },
-  plugins: {
-    title: {
-      text: 'Daily Country Data',
-      display: true
-    },
-    legend: {
-      position: 'right'
-    }
-  }
-});
-
-const chartData = ref({
-  labels: [],
-  datasets: []
-});
-
-const fuelTypes: any = {
-  'UNLEADED_95': {
-    description: 'Αμόλυβδη 95',
-    color: '#003d5c'
-  },
-  'UNLEADED_100': {
-    description: 'Αμόλυβδη 100',
-    color: '#464c89'
-  },
-  'SUPER': {
-    description: 'Super',
-    color: '#954e9b'
-  },
-  'DIESEL': {
-    description: 'Diesel',
-    color: '#dd4d88'
-  },
-  'DIESEL_HEATING': {
-    description: 'Diesel Θέρμανσης',
-    color: '#ff6b59'
-  },
-  'GAS': {
-    description: 'Υγραέριο',
-    color: '#ffa600'
-  }
-};
+const dates = ref([]);
 
 // Get the available date range when the component is mounted
 onMounted(() => {
@@ -83,32 +20,6 @@ onMounted(() => {
     dates.value = [startDate, data.endDate];
   });
 });
-
-watch(dates, async ([startDate, endDate]) => {
-  API.dailyCountryData(startDate, endDate).then(data => {
-    const labels: string[] = [];
-    const fuelTypePrices: Map<string, (string | undefined)[]> = new Map();
-    for (const fuelData of data) {
-      labels.unshift(fuelData.date);
-      for (const fuelType of Object.keys(fuelTypes)) {
-        const price = fuelData.data.find((e) => e.fuel_type === fuelType)?.price;
-        const data = fuelTypePrices.get(fuelType) || [];
-        data.unshift(price)
-        fuelTypePrices.set(fuelType, data);
-      }
-    }
-    const datasets = Object.keys(fuelTypes).map(function (fuelType: string) {
-      return {
-        label: fuelTypes[fuelType].description,
-        data: fuelTypePrices.get(fuelType),
-        backgroundColor: fuelTypes[fuelType].color,
-        borderColor: fuelTypes[fuelType].color
-      };
-    });
-
-    chartData.value = {labels: labels, datasets: datasets}
-  });
-});
 </script>
 
 <template>
@@ -117,14 +28,9 @@ watch(dates, async ([startDate, endDate]) => {
     <VueDatePicker v-model="dates" range multi-calendars :min-date="dateRange.minDate" :max-date="dateRange.maxDate"
                    :time-config="{enableTimePicker: false}" :formats="{ input: 'MMM dd yyyy' }"/>
   </div>
-  <div class="chart">
-    <Line :data="chartData" :options="chartOptions"/>
-  </div>
+  <DailyCountryData :dates="dates"/>
 </template>
 
 <style scoped>
-.chart {
-  height: 600px;
-  width: 80%;
-}
+
 </style>
---
+++ src/components/DailyCountryData.vue
@@ -0,0 +1,114 @@
+<script setup lang="ts">
+import {ref, watch} from 'vue'
+import {Line} from 'vue-chartjs'
+import {
+  CategoryScale, Chart as ChartJS, Legend, LinearScale, LineElement, PointElement, Title, Tooltip, TimeScale
+} from 'chart.js'
+
+import {API} from "@/services/api.ts";
+
+import 'chartjs-adapter-moment';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+
+// Register ChartJS components
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, TimeScale);
+
+const props = defineProps(['dates'])
+
+const fuelTypes: any = {
+  'UNLEADED_95': {
+    description: 'Αμόλυβδη 95',
+    color: '#003d5c'
+  },
+  'UNLEADED_100': {
+    description: 'Αμόλυβδη 100',
+    color: '#464c89'
+  },
+  'SUPER': {
+    description: 'Super',
+    color: '#954e9b'
+  },
+  'DIESEL': {
+    description: 'Diesel',
+    color: '#dd4d88'
+  },
+  'DIESEL_HEATING': {
+    description: 'Diesel Θέρμανσης',
+    color: '#ff6b59'
+  },
+  'GAS': {
+    description: 'Υγραέριο',
+    color: '#ffa600'
+  }
+};
+
+const chartOptions = ref({
+  responsive: true,
+  maintainAspectRatio: false,
+  pointStyle: false,
+  spanGaps: true,
+  scales: {
+    x: {
+      type: 'time',
+      grid: {
+        display: false
+      }
+    },
+  },
+  plugins: {
+    title: {
+      text: 'Daily Country Data',
+      display: true
+    },
+    legend: {
+      position: 'right'
+    }
+  }
+});
+
+const chartData = ref({
+  labels: [],
+  datasets: []
+});
+
+watch(props, async () => {
+  const [startDate, endDate] = props.dates;
+  API.dailyCountryData(startDate, endDate).then(data => {
+    const labels: string[] = [];
+    const fuelTypePrices: Map<string, (string | undefined)[]> = new Map();
+    for (const fuelData of data) {
+      labels.unshift(fuelData.date);
+      for (const fuelType of Object.keys(fuelTypes)) {
+        const price = fuelData.data.find((e) => e.fuel_type === fuelType)?.price;
+        const data = fuelTypePrices.get(fuelType) || [];
+        data.unshift(price)
+        fuelTypePrices.set(fuelType, data);
+      }
+    }
+    const datasets = Object.keys(fuelTypes).map(function (fuelType: string) {
+      return {
+        label: fuelTypes[fuelType].description,
+        data: fuelTypePrices.get(fuelType),
+        backgroundColor: fuelTypes[fuelType].color,
+        borderColor: fuelTypes[fuelType].color
+      };
+    });
+
+    chartData.value = {labels: labels, datasets: datasets}
+  });
+});
+</script>
+
+<template>
+  <div class="chart">
+    <Line :data="chartData" :options="chartOptions"/>
+  </div>
+</template>
+
+<style scoped>
+.chart {
+  height: 600px;
+  width: 80%;
+}
+</style>
