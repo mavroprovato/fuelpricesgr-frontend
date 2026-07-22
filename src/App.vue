692feea4bc4f34c882bd6c54057f6b9eb9483126
Kostas Kokkoros <mavroprovato@gmail.com>
2026-07-22 22:44:58 +0300

    Create a base config for router

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,15 @@
 <script setup lang="ts">
-import {onMounted, ref} from 'vue'
-import {VueDatePicker} from '@vuepic/vue-datepicker';
-import {API} from './services/api.ts';
-import type {DateRange} from './services/api.ts';
-import DailyCountryData from './components/DailyCountryData.vue';
-import DailyPrefectureData from './components/DatePrefectureData.vue';
-import { el } from "date-fns/locale"
-
-/** The date range for the date picker */
-const dateRange = ref<DateRange>({
-  startDate: null,
-  endDate: null
-});
-/** The selected dates from the date picker **/
-const selectedDates = ref<[Date, Date] | []>([]);
-
-/** Called when the component is mounted. Gets the date range for the date picker and the default dates to display */
-onMounted(() => {
-  API.dateRange('daily_country').then(data => {
-    // Set the date range for the date picker
-    dateRange.value = { startDate: data.startDate, endDate: data.endDate }
-    if (data.startDate && data.endDate) {
-      let startDate = new Date(data.endDate);
-      startDate.setMonth(startDate.getMonth() - 3);
-      selectedDates.value = [startDate, data.endDate];
-    }
-  });
-});
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <h1>Τιμές Καυσίμων</h1>
-  <div>
-    <VueDatePicker v-model="selectedDates" range multi-calendars :min-date="dateRange.startDate || ''"
-                   :max-date="dateRange.endDate || ''" :time-config="{enableTimePicker: false}"
-                   :locale="el" :formats="{ input: 'dd MMM yyyy' }"/>
-  </div>
-  <div>
-    <DailyCountryData :selectedDates="selectedDates" />
-  </div>
-  <div>
-    <DailyPrefectureData :date="selectedDates[1]" />
-  </div>
+  <header>
+
+  </header>
+  <main>
+    <h1>Fuel prices</h1>
+    <RouterView />
+  </main>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
+import router from './router'
 
-import 'chartjs-adapter-moment';
+const app = createApp(App)
 
-import '@vuepic/vue-datepicker/dist/main.css'
+app.use(createPinia())
+app.use(router)
 
-createApp(App).mount('#app')
+app.mount('#app')
