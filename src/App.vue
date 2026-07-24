faa10034a03556d0b481c3783d0e4cc9e7ddfd1f
Kostas Kokkoros <mavroprovato@gmail.com>
2026-07-25 02:20:40 +0300

    Set available dates

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,10 @@ import {RouterView} from 'vue-router'
 
 import {VueDatePicker} from '@vuepic/vue-datepicker';
 
-import {API, type DateRange} from '@/services/api.ts';
+import {API} from '@/services/api.ts';
 
 /** The available dates */
-const availableDates = ref<DateRange>({
-  startDate: null,
-  endDate: null
-});
+const availableDates = ref<[Date, Date] | []>([])
 
 /** The selected dates from the date picker **/
 const dateRange = ref<[Date, Date] | []>([]);
@@ -22,6 +19,7 @@ onMounted(() => {
   API.dateRange('daily_country').then(data => {
     // Set the date range for the date picker
     if (data.startDate && data.endDate) {
+      availableDates.value = [data.startDate, data.endDate];
       let startDate = new Date(data.endDate);
       startDate.setMonth(startDate.getMonth() - 3);
       dateRange.value = [startDate, data.endDate];
@@ -34,8 +32,8 @@ onMounted(() => {
   <main>
     <h1><RouterLink to="/">Τιμές Καυσίμων</RouterLink></h1>
     <div>
-      <VueDatePicker v-model="dateRange" range multi-calendars :min-date="availableDates.startDate || ''"
-                     :max-date="availableDates.endDate || ''" :time-config="{enableTimePicker: false}"
+      <VueDatePicker v-model="dateRange" range multi-calendars :min-date="availableDates[0]"
+                     :max-date="availableDates[1]" :time-config="{enableTimePicker: false}"
                      :formats="{ input: 'dd MMM yyyy' }"/>
     </div>
     <RouterView :dateRange="dateRange" />
