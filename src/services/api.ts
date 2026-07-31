b351b77e74ffe69988b2cf47160db30496c65ab4
Kostas Kokkoros <mavroprovato@gmail.com>
2026-08-01 01:24:27 +0300

    Extract method to fetch URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,46 +1,65 @@
+/**
+ * The date range response data
+ */
 export interface DateRange {
     startDate: Date | null;
     endDate: Date | null;
 }
 
+/**
+ * The daily country response data.
+ */
 export interface DailyCountryData {
     date: Date;
     data_file: string;
     data: Array<CountryData>;
 }
 
+/**
+ * The country data.
+ */
 export interface CountryData {
     fuel_type: string;
     price: number;
     number_of_stations: number | null;
 }
 
+/**
+ * The daily prefecture response data.
+ */
 export interface DailyPrefectureData {
     date: Date;
     data_file: string;
     data: Array<PrefectureData>;
 }
 
+/**
+ * The prefecture data.
+ */
 export interface PrefectureData {
     prefecture: string;
     fuel_type: string;
     price: number;
 }
 
+/**
+ * Gets data from the API endpoints.
+ */
 export class API {
+    /** The API base URL */
     static API_BASE: string = import.meta.env.VITE_API_BASE;
 
     static async dateRange(fuelType: string): Promise<DateRange> {
-        const url = new URL(`dateRange/${fuelType}`, API.API_BASE);
+        return await API.#fetchUrl(`dateRange/${fuelType}`).then(
+            async function (response: Response): Promise<DateRange> {
+                const data = await response.json();
 
-        return await fetch(url).then(async function (response: Response): Promise<DateRange> {
-            const data = await response.json();
-
-            return {
-                startDate: data.start_date ? new Date(Date.parse(data.start_date)) : null,
-                endDate: data.end_date ? new Date(Date.parse(data.end_date)) : null
+                return {
+                    startDate: data.start_date ? new Date(Date.parse(data.start_date)) : null,
+                    endDate: data.end_date ? new Date(Date.parse(data.end_date)) : null
+                }
             }
-        });
+        );
     }
 
     static async dailyCountryData(
@@ -48,10 +67,10 @@ export class API {
     ): Promise<Array<DailyCountryData>> {
         const url = new URL('data/daily/country', API.API_BASE);
         if (startDate) {
-            url.searchParams.append('start_date', API.toISODateString(startDate));
+            url.searchParams.append('start_date', API.#toISODateString(startDate));
         }
         if (endDate) {
-            url.searchParams.append('end_date', API.toISODateString(endDate));
+            url.searchParams.append('end_date', API.#toISODateString(endDate));
         }
 
         return await fetch(url).then(async function (response: Response): Promise<Array<DailyCountryData>> {
@@ -67,10 +86,10 @@ export class API {
             url.searchParams.append('prefecture', prefecture);
         }
         if (startDate) {
-            url.searchParams.append('start_date', API.toISODateString(startDate));
+            url.searchParams.append('start_date', API.#toISODateString(startDate));
         }
         if (endDate) {
-            url.searchParams.append('end_date', API.toISODateString(endDate));
+            url.searchParams.append('end_date', API.#toISODateString(endDate));
         }
 
         return await fetch(url).then(async function (response: Response) {
@@ -78,7 +97,20 @@ export class API {
         });
     }
 
-    static toISODateString(date: Date): string {
+    static async #fetchUrl(path: string): Promise<Response> {
+        const url = new URL(path, API.API_BASE);
+
+        return await fetch(url);
+    }
+
+    /**
+     * Formats a date to an ISO date string.
+     *
+     * @param date The date
+     * @return The ISO date string
+     * @private
+     */
+    static #toISODateString(date: Date): string {
         return date.toISOString().split('T')[0] || '';
     }
 }
